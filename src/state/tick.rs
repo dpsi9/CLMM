c5852d6af6543ac6e0ed7911e55920315197a463
Dipendra Singh <dipendras953@gmail.com>
2026-01-11 15:46:18 +0530

    add tick state

---
+++ src/state/tick.rs
@@ -0,0 +1,23 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+
+pub const REWARD_NUM: usize = 3;
+
+#[derive(BorshDeserialize, BorshSerialize, Debug, Clone, Default)]
+#[repr(C)]
+pub struct TickState {
+    pub tick: i32,
+    pub liquidity_net: i128,
+    pub liquidity_gross: u128,
+    pub feee_growth_outside_0_x64: u128,
+    pub fee_growth_outside_1_x64: u128,
+    pub reward_growths_outside_x64: [u128; REWARD_NUM],
+    pub padding: [u32; 13],
+}
+
+impl TickState {
+    pub const LEN: usize = 4 + 16 + 16 + 16 + 16 + 48 + 52;
+
+    pub fn is_initialized(&self) -> bool {
+        self.liquidity_gross > 0
+    }
+}
